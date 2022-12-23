0b6879f50e2a82b5cc45fb6c89168f049dea4af3
Andreia Vieira <andreiavieira2353@gmail.com>
2022-12-23 16:45:51 -0300

    ajustes deploy 2

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -11,17 +11,19 @@ async function searchData(req, res) {
     if (userId.rows.length === 0) {
       return res.sendStatus(404);
     }
+    console.log("aaaa");
 
     const userUrls = await connectionDB.query(
       `SELECT users.id, users.name ,users."visitCount" ,json_agg((urls.id, urls."shortUrl", urls.url, urls."visitCount")) as "shortenedUrls" FROM urls 
-JOIN users ON users.id =
-urls."userId" 
-JOIN sessions ON sessions."userId" = 
-users.id WHERE sessions.token = $1
-GROUP BY users.id
-`,
+      JOIN users ON users.id =
+      urls."userId" 
+      JOIN sessions ON sessions."userId" = 
+      users.id WHERE sessions.token = $1
+      GROUP BY users.id
+      `,
       [token]
     );
+    console.log("bbbbb");
     console.log(userUrls.rows[0]);
 
     if (userUrls.rows.length === 0) {
