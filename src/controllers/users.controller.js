50c609afbd517e7079de80e6828f781499e81c42
Andreia Vieira <andreiavieira2353@gmail.com>
2022-12-23 16:37:51 -0300

    ajuste deploy 1

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -8,12 +8,11 @@ async function searchData(req, res) {
       'SELECT "userId" FROM sessions WHERE token = $1',
       [token]
     );
-    if(userId.rows.length ===0){
+    if (userId.rows.length === 0) {
       return res.sendStatus(404);
     }
 
-
-      const userUrls = await connectionDB.query(
+    const userUrls = await connectionDB.query(
       `SELECT users.id, users.name ,users."visitCount" ,json_agg((urls.id, urls."shortUrl", urls.url, urls."visitCount")) as "shortenedUrls" FROM urls 
 JOIN users ON users.id =
 urls."userId" 
@@ -23,13 +22,21 @@ GROUP BY users.id
 `,
       [token]
     );
-    if(userUrls.rows.length ===0){
-      return res.send(userUrls.rows[0])
-    }
-  const newArray = userUrls.rows[0]?.shortenedUrls.map(item => {return {id: item.f1,ShortUrl:item.f2, url:item.f3, visitCount:item.f4}})
-    
+    console.log(userUrls.rows[0]);
 
-    return res.send({...userUrls.rows[0], shortenedUrls:newArray});
+    if (userUrls.rows.length === 0) {
+      return res.send(userUrls.rows[0]);
+    }
+    const newArray = userUrls.rows[0]?.shortenedUrls.map((item) => {
+      return {
+        id: item.f1,
+        ShortUrl: item.f2,
+        url: item.f3,
+        visitCount: item.f4,
+      };
+    });
+
+    return res.send({ ...userUrls.rows[0], shortenedUrls: newArray });
   } catch (err) {
     res.send(err.message);
   }
